cbd601dcfc32ae042b8e7cbc8fab6def877ee3ad
Daniel Griffin <dgriffinpv@gmail.com>
2023-12-29 00:00:09 -0700

    added dynamism to row component generat ion

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,16 +1,48 @@
+ import { useState } from 'react'
  import Header from'./Header.jsx'
  import Row from './Row.jsx'
  import AddButton from './AddButton.jsx'
 
+ let data = [
+    {
+        gift: 'Stuffed Animal',
+        forWho: 'Qwerty',
+        price: 20,
+        id: 1
+    },
+    {
+        gift: 'Speaker',
+        forWho: 'Rick',
+        price: 50,
+        id: 2
+    },
+    {
+        gift: 'Lego set',
+        forWho: 'Morty',
+        price: 30,
+        id: 3
+    }
+ ]
+
  export default function Table() {
+
+    const [tableData, setTableData] = useState(data)
+
     return (
         <>
             <table>
                 <Header/>
                 <tbody>
-                    <Row gift={'Stuffed Animal'} forWho={'Qwerty'} price={20}/>
-                    <Row gift={'Speaker'} forWho={'Rick'} price={50}/>
-                    <Row gift={'Legos'} forWho={'Morty'} price={30}/>
+                    { tableData.map((giftObj) => {
+                        return <Row
+                            key={giftObj.id}
+                            gift={giftObj.gift}
+                            forWho={giftObj.forWho}
+                            price={giftObj.price}
+                        />
+                    })
+
+                    }
                 </tbody>
             </table>
             <AddButton/>
